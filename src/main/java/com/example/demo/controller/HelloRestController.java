9bef3aa82e742a0a09ae43c57384a73ee03b4218
harsh.misra <harshmishra2801@gmail.com>
2025-03-01 16:44:28 +0530

    Query parameter

--- src/main/java/com/example/demo/controller/HelloRestController.java
+++ src/main/java/com/example/demo/controller/HelloRestController.java
@@ -2,9 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
@@ -13,5 +11,10 @@ public class HelloRestController {
     @RequestMapping(value = {"", "/", "/home"})
     public String sayHello() {
         return "Hello From BridgeLabz!!!";
+    }
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name) {
+
+        return "Hello" + name + "!";
     }
 }
