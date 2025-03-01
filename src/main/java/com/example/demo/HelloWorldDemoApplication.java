4e46a8b317f34848b35d8fe806293845ad2361f5
harsh.misra <harshmishra2801@gmail.com>
2025-03-01 16:41:04 +0530

    Query parameter

--- src/main/java/com/example/demo/HelloWorldDemoApplication.java
+++ src/main/java/com/example/demo/HelloWorldDemoApplication.java
@@ -2,12 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class HelloWorldDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldDemoApplication.class, args);
+    }
+
+//    curl localhost:8080/hello/query?name=Narayan -w "\n"
+
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name) {
+
+        return "Hello" + name + "!";
     }
 }
 
