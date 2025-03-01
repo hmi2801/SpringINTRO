2c8b1dd248af40c23c230d6756ca2fc5ee1d4f84
harsh.misra <harshmishra2801@gmail.com>
2025-03-01 16:58:29 +0530

    post request

--- src/main/java/com/example/demo/controller/HelloRestController.java
+++ src/main/java/com/example/demo/controller/HelloRestController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -23,5 +24,18 @@ public class HelloRestController {
     public String sayHelloParam(@PathVariable String name) {
         return "Hello" + name + "!";
     }
+
+    // curl -X POST -H "Content-Type: application/json"
+//-d '{"firstName": "Lisa", "lastName": "Harry"}'
+//"http://localhost:8080/hello/post"
+    @PostMapping("/post")
+    public String sayHello(@RequestBody User user) {
+        return "Hello" + user.getFirstName() + " " + user.getLastName() + "!";
+    }
 }
+
+
+
+
+
 
